ebc4a9a97b9a93a19eb04522d92b657b704f39d1
SNException <thexsalkin@gmail.com>
2021-08-21 12:56:33 +0200

    multi-threading

--- src/jout.java
+++ src/jout.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.nio.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.*;
 import java.util.function.*;
 import java.util.stream.*;
 
@@ -34,26 +36,47 @@ public final class jout {
         }
     }
 
-    private static String[] listAllDirectories(final Path dir) {
-        if (Files.notExists(dir)) {
-            return null;
-        }
+    private static ThreadPoolExecutor createThreadPool(final int n) {
+        final ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(n);
+        threadPool.setThreadFactory(new ThreadFactory() {
+            @Override
+            public Thread newThread(final Runnable runnable) {
+                final Thread thread = new Thread(runnable);
+                thread.setDaemon(true);
+                return thread;
+            }
+        });
+        return threadPool;
+    }
 
-        try (final Stream<Path> stream = Files.walk(dir, Integer.MAX_VALUE)) {
-            final List<String> files = stream.map(String::valueOf).sorted().collect(Collectors.toList());
-            final ArrayList<String> result = new ArrayList<>(files.size());
+    private static List<List<Object>> slice(final Object[] source, final int chunks) {
+        assert source != null;
+        assert source.length >= chunks;
 
-            for (int i = 0, l = files.size(); i < l; ++i) {
-                final String file = files.get(i);
-
-                if (Files.isDirectory(Paths.get(file))) {
-                    result.add(file);
+        final List<List<Object>> slices = new ArrayList<>(chunks * 2); {
+            int chunkCounter = 0;
+            List<Object> slice = new ArrayList<>(chunks);
+            for (int i = 0; i < source.length; ++i) {
+                slice.add(source[i]);
+                chunkCounter += 1;
+                if (chunkCounter == chunks) {
+                    chunkCounter = 0;
+                    slices.add(slice);
+                    slice = new ArrayList<>();
                 }
             }
-            return result.toArray(String[]::new);
-        } catch (final IOException ex) {
-            return null;
+
+            final int remainder = source.length - (slices.size() * chunks);
+            if (remainder > 0) {
+                final Object[] remainderArray = new Object[remainder];
+                int idx = 0;
+                for (int i = source.length - remainder; i < source.length; ++i) {
+                    remainderArray[idx++] = source[i];
+                }
+                slices.add(Arrays.asList(remainderArray));
+            }
         }
+        return slices;
     }
 
     private static Integer runShellCommand(final StringBuilder buffer, final String...cmdLine) {
@@ -79,8 +102,10 @@ public final class jout {
     }
 
     private static final class ByteCodeInfo {
+
         public final long instCount;
         public final long newInstCount;
+
         public ByteCodeInfo(final long instCount, final long newInstCount) {
             this.instCount    = instCount;
             this.newInstCount = newInstCount;
@@ -125,10 +150,12 @@ public final class jout {
     }
 
     private static final class OutFilesInfo {
+
         public final long binSize;
         public final long outCount;
         public final long innerClassCount;
         public final long anonymousClassCount;
+
         public OutFilesInfo(final long binSize, final long outCount, final long innerClassCount, final long anonymousClassCount) {
             this.binSize             = binSize;
             this.outCount            = outCount;
@@ -166,6 +193,81 @@ public final class jout {
         return new OutFilesInfo(binSize, outCount, innerClassCount, anonymousClassCount);
     }
 
+    private static ByteCodeInfo analyzeByteCode(final String[] files) {
+        final Path javapExecutable = Paths.get(System.getProperty("java.home") + File.separator + "bin" + File.separator + "javap.exe");
+        if (Files.notExists(javapExecutable)) {
+            System.out.println("It seems like you do not java 'javap.exe' installed on your system.");
+            System.exit(1);
+        }
+
+        final int cores = Runtime.getRuntime().availableProcessors();
+        final int threadCount = cores * 100;
+        if (files.length >= threadCount) { // multi thread this
+            final List<List<Object>> slices   = slice(files, threadCount);
+            final ThreadPoolExecutor pool     = createThreadPool(threadCount);
+            final ArrayList<Future<?>> futures = new ArrayList<>();
+
+            final AtomicLong instCount    = new AtomicLong();
+            final AtomicLong newInstCount = new AtomicLong();
+
+            for (final List<Object> slice : slices) {
+                final String[] javapCommandLine = buildJavapCommandLine(javapExecutable.toAbsolutePath().toString(), slice.toArray(String[]::new));
+                // System.out.println(Arrays.toString(javapCommandLine));
+                final Future<?> future = pool.submit(() -> {
+                    final StringBuilder buffer = new StringBuilder();
+                    final Integer code = runShellCommand(buffer, javapCommandLine);
+                    if (code != null && code == 0) {
+                        final ByteCodeInfo info = parseJavapOutput(buffer);
+                        instCount.addAndGet(info.instCount);
+                        newInstCount.addAndGet(info.newInstCount);
+                    } else {
+                        System.out.printf("Failed to gather bytecode information.\n");
+                        System.exit(1);
+                    }
+                });
+                futures.add(future);
+            }
+
+            // block until all threads are done
+            for (final Future<?> future : futures) {
+                try {
+                    future.get();
+                }  catch (final ExecutionException ex) {
+                    ex.printStackTrace(System.err);
+                } catch (final InterruptedException ex) {
+                    assert false : "Not supposed to interrupt this!";
+                }
+            }
+
+            pool.shutdown();
+
+            return new ByteCodeInfo(instCount.get(), newInstCount.get());
+        } else { // single thread this
+            final String[] javapCommandLine = buildJavapCommandLine(javapExecutable.toAbsolutePath().toString(), files);
+            final StringBuilder buffer = new StringBuilder();
+            final Integer code = runShellCommand(buffer, javapCommandLine);
+            if (code != null && code == 0) {
+                return parseJavapOutput(buffer);
+            } else {
+                System.out.printf("Failed to gather bytecode information.\n");
+                System.exit(1);
+            }
+        }
+        return null; // that is not possible I think...
+    }
+
+    private static String[] buildJavapCommandLine(final String exe, final String[] files) {
+        final String[] javapCommandLine = new String[3 + files.length];
+        javapCommandLine[0] = exe;
+        javapCommandLine[1] = "-c";
+        javapCommandLine[2] = "-p";
+        int cursor = 3;
+        for (final String file : files) {
+            javapCommandLine[cursor++] = file;
+        }
+        return javapCommandLine;
+    }
+
     public static void main(final String[] args) {
         if (args.length != 1) {
             System.out.println("Usage:");
@@ -179,39 +281,16 @@ public final class jout {
             System.exit(1);
         }
 
-        final String[] outDirs = listAllDirectories(baseOutDirectory);
-        if (outDirs == null) {
-            System.out.printf("Failed to get all directories inside '%s'.\n", baseOutDirectory.toString());
+        final String[] files = listAllFiles(baseOutDirectory, ".class");
+        if (files == null) {
+            System.out.printf("Failed to get all files inside '%s'.\n", baseOutDirectory.toString());
             System.exit(1);
         }
 
         System.out.println("Gathering information...");
         System.out.println();
 
-        final StringBuilder buffer = new StringBuilder();
-        final String[] javapCommandLine = new String[3 + outDirs.length];
-        final Path javapExecutable = Paths.get(System.getProperty("java.home") + File.separator + "bin" + File.separator + "javap.exe");
-        if (Files.notExists(javapExecutable)) {
-            System.out.println("It seems like you do not java 'javap.exe' installed on your system.");
-            System.exit(1);
-        }
-        javapCommandLine[0] = javapExecutable.toAbsolutePath().toString();
-        javapCommandLine[1] = "-c";
-        javapCommandLine[2] = "-p";
-        int cursor = 3;
-        for (final String dir : outDirs) {
-            javapCommandLine[cursor++] = dir + File.separator + "*.class";
-        }
-        final int code = runShellCommand(buffer, javapCommandLine);
-
-        ByteCodeInfo byteCodeInfo = null;
-        if (code == 0) {
-            byteCodeInfo = parseJavapOutput(buffer);
-        } else {
-            System.out.printf("Failed to gather bytecode information.\n");
-            System.exit(1);
-        }
-
+        final ByteCodeInfo byteCodeInfo = analyzeByteCode(files);
         final OutFilesInfo outFilesInfo = analyzeOutFiles(baseOutDirectory);
 
         System.out.printf("Total output size is in kilobytes        : %.1f\n", outFilesInfo.binSize / 1024.0d);
